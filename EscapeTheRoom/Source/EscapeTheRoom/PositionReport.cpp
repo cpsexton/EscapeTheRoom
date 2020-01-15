8f35d387c0c6ea97732f43a5787098240d8e4f1c
Christopher Sexton <cpsexton@gmail.com>
2020-01-15 11:12:14 -0500

    adds output console messages to position report file

--- EscapeTheRoom/Source/EscapeTheRoom/PositionReport.cpp
+++ EscapeTheRoom/Source/EscapeTheRoom/PositionReport.cpp
@@ -1,6 +1,6 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-
+#include "EscapeTheRoom.h"
 #include "PositionReport.h"
 
 // Sets default values for this component's properties
@@ -8,9 +8,9 @@ UPositionReport::UPositionReport()
 {
 	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
 	// off to improve performance if you don't need them.
+	
+	//bWantsBeginPlay = true;    this is in course file but not mine. (first seen in runtime messages for Feedback
 	PrimaryComponentTick.bCanEverTick = true;
-
-	// ...
 }
 
 
@@ -19,8 +19,7 @@ void UPositionReport::BeginPlay()
 {
 	Super::BeginPlay();
 
-	// ...
-	
+	UE_LOG(LogTemp, Warning, TEXT("Position report reporting for duty!"));	
 }
 
 
