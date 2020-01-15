8a8b687789fee5b960ab21031d2abac8febc0179
Christopher Sexton <cpsexton@gmail.com>
2020-01-14 20:32:51 -0500

    adds initial Unreal and VS files

---
+++ EscapeTheRoom/Source/EscapeTheRoom/EscapeTheRoomGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "EscapeTheRoomGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ESCAPETHEROOM_API AEscapeTheRoomGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
